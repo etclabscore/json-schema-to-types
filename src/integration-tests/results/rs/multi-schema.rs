30e6a1d9e9ef646a18ef02a31fc834bddaba5a8f
Zachary Belford <belfordz66@gmail.com>
2019-11-05 16:26:58 -0800

    fix: duplicate issue
    fixes #26

--- src/integration-tests/results/rs/multi-schema.rs
+++ src/integration-tests/results/rs/multi-schema.rs
@@ -4,18 +4,16 @@ extern crate serde_json;
 ///
 /// a bunch of pets
 ///
-pub type Pets = Vec<OneOfBearManPigSb42HoGS>;
-pub type Bear = serde_json::Value;
-pub type Man = serde_json::Value;
-pub type Pig = serde_json::Value;
+pub type Pets = Vec<OneOfStringWxzVcTo3StringWxzVcTo3VIyVNwmv>;
+pub type StringWxzVcTo3 = String;
 #[derive(Serialize, Deserialize)]
-pub enum OneOfBearManPigSb42HoGS {
-    Bear,
-    Man,
-    Pig
+pub enum OneOfStringWxzVcTo3StringWxzVcTo3VIyVNwmv {
+    StringWxzVcTo3,
+    StringWxzVcTo3
 }
 /// StringXdEiCCNL
 ///
 /// simplest test case
 ///
 pub type StringXdEiCCNL = String;
+pub type Man = serde_json::Value;
