368f61690dcf9a53cafbf65632b8304d6d79314f
Zachary Belford <belfordz66@gmail.com>
2019-11-13 15:32:04 -0800

    fix: remove extra imports

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import TypescriptGenerator from "./codegens/typescript";
 import RustGenerator from "./codegens/rust";
 import GolangGenerator from "./codegens/golang";
 import { ensureSubschemaTitles } from "./ensure-subschema-titles";
-import { capitalize, schemaToRef, sortKeys, sortSchemasByTitle, ensureSchemaTitles } from "./utils";
+import { capitalize, ensureSchemaTitles } from "./utils";
 import { CodeGen } from "./codegens/codegen";
 
 const joinTitles = (s: Schema[]): string => s.map(({ title }: Schema) => title).join("_");
