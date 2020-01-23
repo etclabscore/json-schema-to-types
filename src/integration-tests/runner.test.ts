4efa066a9fdd594a8c023c11a29919f85575795f
Zachary Belford <belfordz66@gmail.com>
2020-01-23 02:28:59 -0800

    feat: greatly improve integration test suite
    adds a generator cli for adding new tests

--- src/integration-tests/runner.test.ts
+++ src/integration-tests/runner.test.ts
@@ -3,36 +3,93 @@ import { JSONSchema } from "@open-rpc/meta-schema";
 import JsonSchemaToTypes from "../index";
 import { capitalize } from "../utils";
 import refParser from "json-schema-ref-parser";
+import { promisify } from "util";
+
+import { getAvailableLanguages, getTestCasesNames } from "./helpers";
+
+interface TestCase {
+  name: string;
+  language: string;
+  expectedTypings: Promise<string>;
+  schema: Promise<JSONSchema>;
+}
+
+const [readDir, readFile] = [promisify(fs.readdir), promisify(fs.readFile)];
+
+const resultsDir = `${__dirname}/results`;
+const testCaseDir = `${__dirname}/test-cases`;
+
+const ensureAllHaveExpectedResult = async (names: string[], languages: string[]) => {
+  const result = await Promise.all(languages.map(async (lang: string) => {
+    const resultsForLang = await readDir(`${resultsDir}/${lang}`);
+    const missingResultsForLang = resultsForLang.filter((r) => names.indexOf(r.replace(`.${lang}`, "")) === -1);
+    if (missingResultsForLang.length > 0) {
+      throw new Error(`missing test case results for language: ${missingResultsForLang}`);
+    }
+  }));
+  if (result.length > 0) { return true; }
+  return false;
+};
+
+const getTestCases = async (names: string[], languages: string[]): Promise<TestCase[]> => {
+  const unflattedTestCases = await Promise.all(
+    languages.map((language) => {
+      return Promise.all(names.map(async (name) => ({
+        name,
+        language,
+        expectedTypings: readFile(`${resultsDir}/${language}/${name}.${language}`, "utf8").then((s) => s.trim()),
+        schema: refParser.dereference(
+          JSON.parse(await readFile(`${testCaseDir}/${name}.json`, "utf8")),
+        ) as Promise<JSONSchema>,
+      })));
+    }),
+  );
+  return unflattedTestCases.reduce((m, tcs) => [...m, ...tcs], []);
+};
 
 describe("Integration tests", () => {
-  let testCases: { [key: string]: JSONSchema };
-  beforeAll(() => {
-    const testCaseDir = `${__dirname}/test-cases`;
-    const testCaseFilenames: string[] = fs.readdirSync(testCaseDir);
-    testCases = testCaseFilenames
-      .reduce((m, caseFilename) => ({
-        ...m,
-        [caseFilename.replace(".json", "")]: refParser.dereference(
-          JSON.parse(fs.readFileSync(`${testCaseDir}/${caseFilename}`, "utf8")),
-        ),
-      }), {});
-    return Promise.all(Object.entries(testCases).map(([n, promise]) => promise));
+  let languages: any;
+  let testCaseNames: string[];
+  let testCases: TestCase[];
+
+  beforeAll(async () => {
+    try {
+      languages = await getAvailableLanguages();
+    } catch (e) {
+      console.error("Error in integration test setup: getAvailableLanguages");
+      console.error(e);
+      throw e;
+      // process.exit(1);
+    }
+    try {
+      testCaseNames = await getTestCasesNames();
+    } catch (e) {
+      console.error("Error in integration test setup: getTestCasesNames");
+      console.error(e);
+      throw e;
+      // process.exit(1);
+    }
+    try {
+      testCases = await getTestCases(testCaseNames, languages);
+    } catch (e) {
+      console.error("Error in integration test setup: getTestCases");
+      console.error(e);
+      throw e;
+      // process.exit(1);
+    }
   });
 
-  const resultsDir = `${__dirname}/results`;
-  const languages: string[] = fs.readdirSync(resultsDir);
+  it("can load the test languages", () => {
+    return expect(ensureAllHaveExpectedResult(testCaseNames, languages)).resolves.toBe(true);
+  });
 
   it("checks out", async () => {
-    const entries = Object.entries(testCases);
-    expect.assertions(entries.length * languages.length);
+    expect.assertions(testCases.length);
 
-    languages.forEach((lang) => {
-      entries.forEach(async ([name, schema]: any) => {
-        const result = fs.readFileSync(`${resultsDir}/${lang}/${name}.${lang}`, "utf8").trim();
-        const transpiler = new JsonSchemaToTypes(await schema);
-        const typings = transpiler[`to${capitalize(lang)}`]();
-        expect(typings).toBe(result);
-      });
+    testCases.forEach(async (testCase: TestCase) => {
+      const transpiler = new JsonSchemaToTypes(await testCase.schema);
+      const typings = transpiler[`to${capitalize(testCase.language)}`]();
+      expect(typings).toBe(await testCase.expectedTypings);
     });
   });
 });
